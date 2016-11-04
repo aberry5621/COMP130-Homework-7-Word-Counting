512e82b26f722090f7c35a24dbcbaae02872277c
aberry5621 <aberry5621@gmail.com>
2016-11-04 09:23:19 -0700

    Initial Commit

---
+++ COMP130 Homework 7 Word Counting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Homework 7 Word Counting
+//
+//  Created by ax on 11/4/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
